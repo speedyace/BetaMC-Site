771d12cb84b78a08b9f9ee3c6f6b0fdbfb344660
SvGaming <sven.kovac1@outlook.com>
2025-01-25 23:57:25 +0100

    add custom random splashes

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function onPageLoaded() {
-    // Write your javascript code here
-    console.log("page loaded");
+    
+
+    const splashes = [
+        "True and Real!",
+        "ferfer",
+        "zuh",
+        "guh",
+        "CruxyMC is always right",
+        "betamc.org"
+    ]
+
+    let splashnumber = Math.floor(Math.random() * splashes.length);
+
+    splash = splashes[splashnumber]
+    document.querySelector("#splashText").innerHTML = splash
 }
